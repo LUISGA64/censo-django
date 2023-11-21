c64a2db949e1beb4772d32f4c83c6ab0bca03b14
LUISGA64 <luisgalic64@gmail.com>
2023-11-20 22:12:20 -0500

    Entidades Asociacion y Organizacion creadas

--- censoapp/models.py
+++ censoapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Association(models.Model):
+    association_name = models.CharField(max_length=50, null=False, blank=False)
+    association_identification = models.CharField(max_length=15, null=False, blank=False)
+    association_type_document = models.CharField(max_length=3, null=False, blank=False, default='NIT',
+                                                 help_text="Ingrese NIT")
+    association_phone_mobile = models.CharField(max_length=15, help_text="Ingrese número de celular")
+    association_phone = models.CharField(max_length=15)
+    association_address = models.CharField(null=False, blank=False)
+    association_departament = models.CharField(null=False, blank=False, max_length=15,
+                                               help_text="Registre el departamento")
+    association_email = models.EmailField(blank=False, null=False)
+    association_logo = models.CharField(null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.association_name} {self.association_identification}"
+
+
+class Organizations(models.Model):
+    organization_name = models.CharField(max_length=50, null=False, blank=False)
+    organization_identification = models.CharField(max_length=15, blank=False, null=False)
+    organization_type_identification = models.CharField(max_length=3, default='NIT')
+    organization_territory = models.CharField(max_length=50, blank=False, null=False)
+    organization_email = models.EmailField(null=False, blank=False)
+    organization_mobile_phone = models.CharField(max_length=15)
+    organization_phone = models.CharField(max_length=15)
+    organization_address = models.CharField(max_length=50, null=False, blank=False)
+    organization_logo = models.CharField()
+    association_id = models.ForeignKey('Association', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.organization_name
