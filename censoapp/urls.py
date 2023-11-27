b58c6676c2442fa9dabe811781cb160af433ca24
LUISGA64 <luisgalic64@gmail.com>
2023-11-27 17:28:06 -0500

    Login!

--- censoapp/urls.py
+++ censoapp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import (index)
+from .views import home, association, LoginView
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
-    path("", index, name="index"),
+    path('', home, name='home'),
+    path('login/', LoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(next_page='accounts/log'), name="logout"),
+    path("asociacion/", association, name="asociacion"),
+
 ]
--- censoapp/views.py
+++ censoapp/views.py
@@ -1,6 +1,24 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+
 
 # Create your views here.
-def index(request):
-    data = request.session['moticas'] = 'hola'
+
+class LoginView(LoginView):
+    template_name = 'index.html'
+    field = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def home(request):
+    return render(request, 'registration/login.html')
+
+
+@login_required
+def association(request):
     return render(request, 'censo/base_site.html')
